fdbe746f904f87813bf0797ab62cf348045c462e
abhikumar63 <63abhikumar@gmail.com>
2022-10-08 14:34:19 +0530

    Complete

--- sketch.js
+++ sketch.js
@@ -4,8 +4,22 @@ let step = 1;
 let numSteps = 1;
 let state = 0;
 let turnCounter = 1;
+
+// Scale / Resolution
 let stepSize = 20;
 let totalSteps;
+
+
+// Function to Check For Prime.
+function isPrime(value){
+    if(value == 1) return false;
+    for(let i=2;i<sqrt(value);i++) {
+        if(value%i == 1) {
+            return false;
+        }
+    }
+    return true;
+}
 
 function setup() {
     createCanvas(500,500);
@@ -21,14 +35,20 @@ function setup() {
 }
 
 function draw() {
-    fill(255);
-    stroke(255);
-    circle(x,y,stepSize*0.5);
 
+    //If Prime, Draw Circle.
+    if(isPrime(step)) {
+        fill(255);
+        stroke(255);
+        circle(x,y,stepSize*0.5);
+    }
+
+    // For Connecting to the previous point with a line.
     line(x,y,px,py);
     px = x;
     py = y;
 
+    // Turning according to state.
     switch (state) {
         case 0:
             x += stepSize;
@@ -44,6 +64,7 @@ function draw() {
             break;
     }
 
+    // Change State.
     if(step%numSteps == 0){
         state = (state+1) % 4;
         turnCounter++;
@@ -53,6 +74,7 @@ function draw() {
     }
     step++;
 
+    // Are we Done?
     if(step > totalSteps) {
         noLoop();
     }
